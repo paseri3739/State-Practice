8fd073f3da58dcddb59b17e2648c6e816eeba835
paseri3739 <tael43@icloud.com>
2025-07-19 14:42:36 +0900

    コメント

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         DoorContext door = new DoorContext();
-
+        // setterを用意してしまうとこの辺で勝手に状態を書き換えられてしまう。
         door.handleEvent(DoorEvent.PRESS_BUTTON);        // Closed → Opening
         door.handleEvent(DoorEvent.FULLY_OPENED);        // Opening → Opened
         door.handleEvent(DoorEvent.PRESS_BUTTON);        // Opened → Closing
